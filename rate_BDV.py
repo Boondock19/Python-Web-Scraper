fec7ea6c59d061e228ae209a1c5b97878762779d
BoondockGCF <jgonzalez@gcf-corp.com>
2022-10-04 12:29:11 -0400

    Agregada libreria de timeloop

--- rate_BDV.py
+++ rate_BDV.py
@@ -2,14 +2,18 @@ from bs4  import BeautifulSoup
 import requests
 import time
 import json
+from timeloop import Timeloop
+from datetime import timedelta
 
 
+tl = Timeloop()
 
 html_file = requests.get('https://www.bcv.org.ve/', verify=False).text
 soup = BeautifulSoup(html_file, 'lxml')
 url = 'https://gcfprojectusuario-default-rtdb.firebaseio.com/projects/proj_k69a1pcZoyHutm7c9DDdx4/data/QA/QA/TasaDolar/0.json'
 
 # Obtener div con valores de USD
+@tl.job(interval=timedelta(seconds=30))
 def get_rate():
     with open ('dolar_BCV.txt', 'w') as f: 
         usd_span = soup.find('span', text=' USD')
@@ -30,8 +34,11 @@ def get_rate():
 
         print(requests.get(url).json())
 
-if __name__ == '__main__':
-    get_rate()
-    time_wait = 1
-    print(f'Waiting {time_wait} minutes...')
-    time.sleep(time_wait * 60)
+# if __name__ == '__main__':
+#     get_rate()
+#     time_wait = 1
+#     print(f'Waiting {time_wait} minutes...')
+#     time.sleep(time_wait * 60)
+
+if __name__ == "__main__":
+    tl.start(block=True)
